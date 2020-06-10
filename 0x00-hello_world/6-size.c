859282f8606b9658adfb4bfa7295d2bb582932a7
santiagopinzonD <santiagopinzon456@gmail.com>
2020-06-10 23:08:05 +0000

    2

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,7 +15,7 @@ long long int long2;
 float float1;
 
 printf("Size of a char: %d byte(s)\n", sizeof(char1));
-printf("Size of a int: %d byte(s)\n", sizeof(int1));
+printf("Size of an int: %d byte(s)\n", sizeof(int1));
 printf("Size of a long int: %d byte(s)\n", sizeof(long1));
 printf("Size of a long long int: %d byte(s)\n", sizeof(long2));
 printf("Size of a float: %d byte(s)\n", sizeof(float1));
