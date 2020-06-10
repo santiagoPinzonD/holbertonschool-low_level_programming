e9d5cba087bf40bc61c5faf9dec56fd03cf9442f
santiagopinzonD <santiagopinzon456@gmail.com>
2020-06-10 23:02:57 +0000

    hello

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,15 +7,18 @@
  */
 int main(void)
 {
+
 char char1;
 int int1;
 long int long1;
 long long int long2;
 float float1;
-printf("size of a char: %d byte(s)\n", sizeof(char1));
-printf("size of a int: %d byte(s)\n", sizeof(int1));
-printf("size of a long int: %d byte(s)\n", sizeof(long1));
-printf("size of a long long int: %d byte(s)\n", sizeof(long2));
-printf("size of a float: %d byte(s)\n", sizeof(float1));
+
+printf("Size of a char: %d byte(s)\n", sizeof(char1));
+printf("Size of a int: %d byte(s)\n", sizeof(int1));
+printf("Size of a long int: %d byte(s)\n", sizeof(long1));
+printf("Size of a long long int: %d byte(s)\n", sizeof(long2));
+printf("Size of a float: %d byte(s)\n", sizeof(float1));
 return (0);
+
 }
