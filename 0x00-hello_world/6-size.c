2bd750f20be10813800fc6d291df8cb84aa488ad
santiagopinzonD <santiagopinzon456@gmail.com>
2020-06-10 22:33:12 +0000

    hello

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char char;
+int int;
+long long;
+long;
+printf("size of a char: %d byte(s)\n", sizeof(char));
+printf("size of a char: %d byte(s)\n", sizeof(int));
+printf("size of a char: %d byte(s)\n", sizeof(long));
+printf("size of a char: %d byte(s)\n", sizeof(long long));
+return (0);
+}
