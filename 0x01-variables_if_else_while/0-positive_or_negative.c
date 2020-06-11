1de3d89dea55c281cadd05d4b1c7663357f4148f
santiagopinzonD <santiagopinzon456@gmail.com>
2020-06-11 21:38:29 +0000

    hello

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,10 +12,10 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
-printf("%i is positive\n", n);
+printf("%d is positive\n", n);
 if (n == 0)
-printf("%i is zero\n", n);
+printf("%d is zero\n", n);
 if (n < 0)                                                                                                           
-printf("%i is negative\n", n);
+printf("%d is negative\n", n);
 return (0);
 }
