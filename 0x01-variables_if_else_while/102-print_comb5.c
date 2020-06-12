42d1be0cb231c83a3f34281edb1fc87e7dc5503a
santiagopinzonD <santiagopinzon456@gmail.com>
2020-06-12 04:46:23 +0000

    hello

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+* main - Printing combinations 5
+* Return: Always 0 (Success)
+**/
+int main(void)
+{
+int n;
+int u;
+int m;
+int e;
+
+for (n = '0'; n <= '9'; n++)
+{
+for (u = '0'; u <= '9'; u++)
+{
+for (m = n; m <= '9'; m++)
+{
+for (e = u + 1 ; e <= 57; e++)
+{
+putchar(n);
+putchar(u);
+putchar(' ');
+putchar(m);
+putchar(e);
+if (n != '9' || u != '8' || m != '9' || e != '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
