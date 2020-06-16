44f891702ea9fd45654d928b253eed679075cade
santiagopinzonD <santiagopinzon456@gmail.com>
2020-06-16 16:30:15 +0000

    hello

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,14 +2,21 @@
 
 /**
 * print_last_digit - check the code for Holberton School students.
-*
+* @r: the number check
 * Return: Always 0.
 */
-int print_last_digit(int n)
+int print_last_digit(int r)
 {
-	if ()
+	int rest = r % 10;
 
-
-
+	if (rest < 0)
+{
+	_putchar (-rest + '0');
+	return (-rest);
 }
-
+	else
+{
+	_putchar (rest + '0');
+	return (rest);
+}
+}
