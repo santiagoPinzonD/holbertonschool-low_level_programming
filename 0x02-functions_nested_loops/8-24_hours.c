50a409bd30fa802609ad28dec5d9cce7596a494f
santiagopinzonD <santiagopinzon456@gmail.com>
2020-06-16 19:47:33 +0000

    hello

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+
+/**
+* print_last_digit - check the code for Holberton School students.
+* @r: the number check
+* Return: Always 0.
+*/
+
+	void jack_bauer(void)
+{
+	int h;
+	int m;
+	int j;
+	int k;
+	int t = 9;
+
+	for (h = 0; h <= 2; h++)
+{
+	if( h == 2)
+	t = 3;
+	
+	for (m = 0; m <= t; m++)
+{
+	for (j = 0; j <= 5; j++)
+{
+	for (k = 0; k <= 9; k++)
+{
+	_putchar(h + '0');
+	_putchar(m + '0');
+	_putchar(':');
+	_putchar(j + '0');
+	_putchar(k + '0');
+	_putchar('\n');
+}
+}
+}
+}
+}
