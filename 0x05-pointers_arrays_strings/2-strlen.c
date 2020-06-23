1bc83d60fe5ec7aaf0ae4dbfaa134f2a45eb139b
santiagopinzonD <santiagopinzon456@gmail.com>
2020-06-23 16:49:17 +0000

    hello

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,17 +1,20 @@
 #include "holberton.h"
 
 /**
-* _strlen - check the code for Holberton School students.
-* @s: this is a pointer
-* Return: Always 0.
+* _strlen - return the longitud
+*
+* @s: this is for check
+*
+* Return: p
 */
 
 int _strlen(char *s)
 {
-	int x;
+	int p;
 
-	for (x = 0; s[x] != '\0'; x++)
-{
-}
-	return (x);
+	for (p = 0; *s != '\0'; p++)
+	{
+		s++;
+	}
+	return (p);
 }
