a4c0fc2a0b997c6fc92f391ed8bc0fb23eed73c9
santiagopinzonD <santiagopinzon456@gmail.com>
2020-06-24 01:46:37 +0000

    copy the value of pointer

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+* *_strcpy - check the code for Holberton School students.
+* @dest: this a pointer
+* @src: this is a pointer
+* Return: Always 0.
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	int c;
+	char a;
+
+	for (c = 0; src[c] != '\0'; c++)
+	{
+		a = src[c];
+		dest[c] = a;
+	}
+	return (dest);
+}
