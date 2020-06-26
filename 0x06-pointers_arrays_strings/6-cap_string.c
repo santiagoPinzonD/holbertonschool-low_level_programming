41742c01d4a9995b59fe9142d90cf22d55a9ee93
santiagopinzonD <santiagopinzon456@gmail.com>
2020-06-26 05:18:33 +0000

    hello

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+
+/**
+ * *cap_string - check the code for Holberton School students.
+ *
+ * Return: char
+ */
+
+char *cap_string(char *a)
+{
+	int x;
+
+	x = 0;
+
+	while (a[x] != '\0')
+	{
+		if (a[0] >= 97 && a[0] <= 122)
+		{
+			a[0] -= 32; 
+		}
+		if (a[x] >= 97 && a[x] <= 122 && (a[x -1] == ',' || a[x - 1] == ';'
+				|| a[x - 1] == '.' || a[x - 1] == '!' || a[x - 1] == '?' || a[x - 1] == '"'
+				|| a[x - 1] == '(' || a[x - 1] == ')' || a[x - 1] == '{' || a[x - 1] == '}' 
+				|| a[x - 1] == ' ' || a[x - 1] == 32 || a[x-1] == '\t'))
+		{
+			a[x] -= 32;
+		}
+		x++;
+
+	}
+	return (a);
+}
