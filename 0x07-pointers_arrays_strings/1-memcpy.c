8fb4e3e457c9e5c1c880f3baa68a2d0d3a074d84
santiagopinzonD <santiagopinzon456@gmail.com>
2020-06-29 16:33:30 +0000

    hello

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+* *_memcpy - check the code for Holberton School students.
+* @dest: donde se va a copiar lo de sr
+* @src: memria que se va a copiar
+* @n: lo que se llena
+* Return: Always 0.
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	long int x;
+
+	for (x = 0; x < n; x++);
+	{
+		*dest = *src;
+	}
+	return (dest); 
+}
