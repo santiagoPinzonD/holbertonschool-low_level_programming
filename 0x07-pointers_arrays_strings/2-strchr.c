3e16fd9f00c4e3db90717408ed560d93e5204220
santiagopinzonD <santiagopinzon456@gmail.com>
2020-06-29 20:12:30 +0000

    hello

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * *_strchr - check the code for Holberton School students.
+ * @s: thi is a pointer
+ * @c: thi is a variable type of char
+ * Return: s
+ */
+
+char *_strchr(char *s, char c)
+{
+	int x;
+
+	for (; s[x] != '\0'; x++)
+	{
+		if (s[x] == c)
+		{
+			return (s + x);
+		}
+	}
+	return ('\0');
+}
