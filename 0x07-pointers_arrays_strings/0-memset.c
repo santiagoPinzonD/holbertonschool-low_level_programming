75dc8a773cb344fae808edfe0725e90c951dc39c
santiagopinzonD <santiagopinzon456@gmail.com>
2020-06-29 16:20:09 +0000

    hello

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,16 +1,17 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
- *
+ * *_memset - check the code for Holberton School students.
+ * @s: es la memoria señalada
+ * @b: es lo que se va llenar la memoria
+ * @n: es la memoria que se va a llenar
  * Return: Always 0.
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-
 	long int a;
-	
+
 	for (a = 0; a < n; a++)
 	{
 		s[a] = b;
