2206ba7c1fc03674e15427d6cf085b8aa3d0f003
santiagopinzonD <santiagopinzon456@gmail.com>
2020-06-30 04:47:10 +0000

    hello

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+* print_chessboard - check the code for Holberton School students.
+* @a: matrix
+* Return: void .
+*/
+
+void print_chessboard(char (*a)[8])
+{
+	int c;
+	int f;
+
+	for (f = 0; f < 8; f++)
+	{
+		for (c = 0; c < 8; c++)
+		{
+			_putchar((*a)[c]);
+		}
+		_putchar('\n');
+		a++;
+	}
+}
