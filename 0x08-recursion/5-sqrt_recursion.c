ba6a259c44d1843f4dac19bfaa6384af0160454f
santiagopinzonD <santiagopinzon456@gmail.com>
2020-07-02 06:50:01 +0000

    hello

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+* _sqrt_recursion - check the code for Holberton School students.
+* @n: i dont know
+* Return: Always 0.
+*/
+
+int _sqrt_recursion(int n)
+{
+	/*define las condiciones inciales de la recursividad */
+	int x = 1;
+	int resultado = 0;
+
+	resultado = calculodelaraiz(n, x);
+	return (resultado);
+}
+
+int calculodelaraiz(int n, int x)
+{
+	if (n / x == x && n % x == 0)
+	{
+		return (x);
+	}
+	if (n / x < x)
+	{
+		return (-1);
+	}
+	x = calculodelaraiz(n, x + 1);
+	return (x);
+}
