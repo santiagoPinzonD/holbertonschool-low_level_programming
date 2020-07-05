f42dcdb299d2739320a2cc6a8fd0f0f60d8ea5e3
santiagopinzonD <santiagopinzon456@gmail.com>
2020-07-05 02:43:14 +0000

    esto es atoi

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,12 +11,12 @@ int main(int argc __attribute__((unused)), char *argv[])
 {
 	if (argc != 3)
 	{
-		printf ("Error\n");
+		printf("Error\n");
 		return (1);
 	}
 	else
 	{
 		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
-		return (0);	
+		return (0);
 	}
 }
