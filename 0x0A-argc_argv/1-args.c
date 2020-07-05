032f2388bdeb4ed25349e38fb51bfb52b66fc478
santiagopinzonD <santiagopinzon456@gmail.com>
2020-07-05 00:34:27 +0000

    question 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - esto es imprimir el argumento 0 que es el name
+* @argc: la cantidad de argumentos
+* @argv: el valor de cada argumento
+* Return: always 0
+*/
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
