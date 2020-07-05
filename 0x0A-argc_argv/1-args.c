e1a4e06f2d2659d746f754edfce6e6fc557dcc8c
santiagopinzonD <santiagopinzon456@gmail.com>
2020-07-05 00:42:16 +0000

    quitar que cuente el parametro de -1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
 * main - esto es imprimir el argumento 0 que es el name
 * @argc: la cantidad de argumentos
@@ -8,6 +9,7 @@
 
 int main(int argc, char *argv[] __attribute__((unused)))
 {
-	printf("%d\n", argc);
+
+	printf("%d\n", argc - 1);
 	return (0);
 }
