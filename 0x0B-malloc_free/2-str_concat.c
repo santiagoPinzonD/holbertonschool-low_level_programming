35ba0479437b8a095771b8176b22309d0b202bd8
santiagopinzonD <santiagopinzon456@gmail.com>
2020-07-08 00:42:22 +0000

    concatenar dos cadenas alojando el espacio

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,45 @@
+#include "holberton.h"
+
+/**
+* str_concat - check the code for Holberton School students.
+* @s1: primera string
+* @s2: segunda string
+* Return: Always 0.
+*/
+char *str_concat(char *s1, char *s2)
+{
+	int z, x, c, d;
+	char *a;
+
+	if (s1 == NULL)
+	{
+		s1 = '\0';
+	}
+	if (s2 == NULL)
+	{
+		s2 = '\0';
+	}
+	for (x = 0; s1[x] != '\0'; x++)
+	{
+
+	}
+	for (z = 0; s2[z] != '\0'; z++)
+	{
+
+	}
+	a = malloc((x + z) * sizeof(char));
+
+	if (a == NULL)
+	{
+		return (NULL);
+	}
+	for (c = 0; c < x; c++)
+	{
+		a[c] = s1[c];
+	}
+	for (d = 0; d < z; d++, c++)
+	{
+		a[c] = s2[d];
+	}
+	return (a);
+}
