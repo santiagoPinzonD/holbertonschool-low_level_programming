da386c0fb2c32b95b436cdf14a09d69387e1b6d5
santiagopinzonD <santiagopinzon456@gmail.com>
2020-07-14 02:29:33 +0000

    condicion si existe

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,16 +1,19 @@
 #include "dog.h"
 
 /**
-* init_dog - check the code for Holberton School students.
-* @d: puntero que apunta a la struct dog
-* @name: un puntero de tipo char
-* @age: edad
-* @owner: propietario
-*/
+ * init_dog - check the code for Holberton School students.
+ * @d: puntero que apunta a la struct dog
+ * @name: un puntero de tipo char
+ * @age: edad
+ * @owner: propietario
+ */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d)
+	{
 		d->name = name;
 		d->age = age;
 		d->owner = owner;
+	}
 }
