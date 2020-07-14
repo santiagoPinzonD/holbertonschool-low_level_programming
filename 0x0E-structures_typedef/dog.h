5fe9f544eee8bb0d75383217da0950f89d50845f
santiagopinzonD <santiagopinzon456@gmail.com>
2020-07-14 01:31:31 +0000

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Descripción breve
+ * @name: primer miembro
+ * @age: segundo miembro
+ * @owner: tercer miembro
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
