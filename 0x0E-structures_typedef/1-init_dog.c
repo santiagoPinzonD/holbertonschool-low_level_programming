387d46f437b87c345877f30a1b50bcf0a5555b85
santiagopinzonD <santiagopinzon456@gmail.com>
2020-07-14 02:27:28 +0000

    inicializar una struct

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+* init_dog - check the code for Holberton School students.
+* @d: puntero que apunta a la struct dog
+* @name: un puntero de tipo char
+* @age: edad
+* @owner: propietario
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,17 +1,21 @@
 #ifndef DOG_H
 #define DOG_H
+#include <stdio.h>
 
 /**
- * struct dog - Descripción breve
- * @name: primer miembro
- * @age: segundo miembro
- * @owner: tercer miembro
- */
+* struct dog - Descripción breve
+* @name: primer miembro
+* @age: segundo miembro
+* @owner: tercer miembro
+*/
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
