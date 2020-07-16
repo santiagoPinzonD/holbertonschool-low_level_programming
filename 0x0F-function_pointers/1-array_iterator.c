ac635ada72273864c2768df894fd9ec1ab68bfc1
santiagopinzonD <santiagopinzon456@gmail.com>
2020-07-16 00:21:13 +0000

    error de betty

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -4,13 +4,14 @@
 * array_iterator - prints an integer
 * @array: arreglo
 * @size: tamaño de matriz
+* @action: puntero que apunta a array
 * Return: Nothing.
 */
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t x;
-	
+
 	if (action && array)
 	{
 		for (x = 0; x < size; x++)
