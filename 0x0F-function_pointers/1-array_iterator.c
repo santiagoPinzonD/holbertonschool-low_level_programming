47b437e13939cb04b5feedd64e4463ad31c4c8d0
santiagopinzonD <santiagopinzon456@gmail.com>
2020-07-16 00:19:09 +0000

    array iterator

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+
+/**
+* array_iterator - prints an integer
+* @array: arreglo
+* @size: tamaño de matriz
+* Return: Nothing.
+*/
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t x;
+	
+	if (action && array)
+	{
+		for (x = 0; x < size; x++)
+		{
+			action(array[x]);
+		}
+	}
+}
