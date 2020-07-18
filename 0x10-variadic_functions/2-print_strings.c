e6adf5e1aa49f24ebf2dcef25c8799477bd8d1be
santiagopinzonD <santiagopinzon456@gmail.com>
2020-07-18 23:02:46 +0000

    imprimir string

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - check the code for Holberton School students.
+ * @separator: coma y espacio
+ * @n: n args
+ * Return: Always 0.
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int x;
+
+	va_start(args, n);
+	for (x = 0; x < n; x++)
+	{
+		printf("%s", va_arg(args, char *));
+		if (separator != NULL && x != (n - 1))
+		{
+			printf("%s", separator);
+		}
+		if (separator == NULL)
+		{
+			printf("(nil)");
+		}
+	}
+	printf("\n");
+	va_end(args);
+}
