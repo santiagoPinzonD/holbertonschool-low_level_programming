11ed3b9df036d72afe2ea78304fd9e7b114ce6ac
santiagopinzonD <santiagopinzon456@gmail.com>
2020-07-24 03:05:45 +0000

    used atribute__constructor

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* xde - check the code for Holberton School students.
+*
+* Return: Always 0.
+*/
+void xde(void)__attribute__((constructor));
+void xde(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
