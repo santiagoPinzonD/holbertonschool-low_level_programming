d3a24270a0144f7750c0282e552e1324e9ac2e1f
santiagopinzonD <santiagopinzon456@gmail.com>
2020-07-28 16:51:46 +0000

    add nodo in the end

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+* add_nodeint_end - check the code for Holberton School students.
+* @n: nodos
+* @head: puntero al primer nodo.
+* Return: Always 0.
+*/
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new;
+	listint_t *aux = *head;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->n = n;
+	new->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+	while (aux->next != NULL)
+	{
+		aux = aux->next;
+	}
+	aux->next = new;
+	return (new);
+}
