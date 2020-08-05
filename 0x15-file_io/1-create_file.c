6026e6ba5496bd2c161ad130887739186adef4f2
santiagopinzonD <santiagopinzon456@gmail.com>
2020-08-05 02:17:24 +0000

    error

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename)
 	{
-		fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
+		fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 		if (fd == -1)
 			return (-1);
 
