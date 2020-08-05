d1a6c8b9e9f2d1d3af39a9df4d81ab7f324927f9
santiagopinzonD <santiagopinzon456@gmail.com>
2020-08-05 02:26:57 +0000

    xde

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-
 /**
  * create_file - check the code for Holberton School students.
  * @filename: pointer to file
@@ -13,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename)
 	{
 		fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
-		if (fd == -1)
+		if (fd < 0)
 			return (-1);
 
 		if (text_content)
