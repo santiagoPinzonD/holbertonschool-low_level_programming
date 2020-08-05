0d48588d7527898ef077a2850c1504e14667e2c7
santiagopinzonD <santiagopinzon456@gmail.com>
2020-08-05 02:00:21 +0000

    created file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * create_file - check the code for Holberton School students.
+ * @filename: pointer to file
+ * @text_content: contain the chars
+ * Return: Always 0.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int x;
+
+	if (filename)
+	{
+		fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+		if (fd < 0)
+			return (-1);
+
+		if (text_content)
+		{
+			for (x = 0; text_content[x] != '\0'; x++)
+			{
+			}
+			write(fd, text_content, x);
+		}
+		close(fd);
+		return (1);
+	}
+	return (-1);
+}
