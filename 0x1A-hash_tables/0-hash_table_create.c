1910454af02064fb22bd8375f251d928cd547ef1
santiagopinzonD <santiagopinzon456@gmail.com>
2020-10-02 19:35:39 +0000

    make changes

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,12 +7,26 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table;
+	hash_node_t **new_node;
+	unsigned long int i;
 
 	new_table = malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
 		return (NULL);
-	if (!new_table)
+
+	new_node = malloc(sizeof(hash_node_t *) * size);
+	if (new_node == NULL)
+	{
 		free(new_table);
+		return (NULL);
+	}
 	new_table->size = size;
+	new_table->array = new_node;
+
+	for (i = 0; i < size; i++)
+	{
+		new_node[i] = NULL;
+	}
+
 	return (new_table);
 }
