b770d8a3b11780dab93533b3275d2a5597cf1924
santiagopinzonD <santiagopinzon456@gmail.com>
2020-10-02 16:54:25 +0000

    fix betty

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,13 +6,13 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *new_table;
+	hash_table_t *new_table;
 
-    new_table = malloc(sizeof(hash_table_t));
-    if (new_table == NULL)
-        return (NULL);
-    if (!new_table)
-        free(new_table);
-    new_table->size = size;
-    return (new_table);
+	new_table = malloc(sizeof(hash_table_t));
+	if (new_table == NULL)
+		return (NULL);
+	if (!new_table)
+		free(new_table);
+	new_table->size = size;
+	return (new_table);
 }
