d38815f56f1401ffe154470754f03f52cfffd476
santiagopinzonD <santiagopinzon456@gmail.com>
2020-10-02 16:52:42 +0000

    key index

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,15 +1,16 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - creates a hash table.
+ * key_index - function that return a index
  * @size: the size of the array
+ * @key: the key passed for main
  * Return: pointer to the newly created hash table.
  */
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
- {
-    unsigned int index;
+{
+	unsigned int index;
 
-    index = (hash_djb2(key) % size);
-    return (index);
- }
+	index = (hash_djb2(key) % size);
+	return (index);
+}
