34dec936a1e3b2277f7b6257bd1ea7d4defc0d96
santiagopinzonD <santiagopinzon456@gmail.com>
2020-11-06 23:01:29 +0000

    firs time

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""module of function"""
+
+
+def island_perimeter(grid):
+    """that returns the perimeter of the island described in grid"""
+    p = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1 and p == 0:
+                p += 4
+                break
+            if grid[i][j] == 1:
+                p += 2
+    return p
+
+
+grid = [[0,1]]
+print(island_perimeter(grid))
